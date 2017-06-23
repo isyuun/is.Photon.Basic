63fae1944815e095224e44fc5f25b25812fc2de1
isyuun <isyuun@gmail.com>
2017-06-23 20:23:11 +0900

    ...

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : ___Plane
+{
+    protected override void Update()
+    {
+        PhotonView pv = gameObject.GetComponent<PhotonView>();
+        //Debug.Log(this.GetMethodName() + ":" + pv.isMine + ":" + pv);
+        if (pv != null && pv.isMine)
+        {
+            base.Update();
+        }
+    }
+}
